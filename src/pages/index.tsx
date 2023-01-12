568f87708275f343a2e7a88a69fc57ba1963f570
duyvinh <duyvinh229148@gmail.com>
2023-01-12 18:26:33 +0700

    Using secret vars

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -66,4 +66,4 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Vinh Do</title>
